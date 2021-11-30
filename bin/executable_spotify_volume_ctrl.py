ba6ea24482a8a3f1d8cd7e50d3036c1e1d0d7e3a
Jorik Cronenberg <jcronenberg@suse.de>
2021-11-30 16:33:35 +0100

    Modify spotify_volume_ctrl script
    Fix issue of multiple playbacks in pactl list when casting to a device

--- bin/executable_spotify_volume_ctrl.py
+++ bin/executable_spotify_volume_ctrl.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python3
 #Author: Marcin Kocur, attribution license: https://creativecommons.org/licenses/by/4.0/
+# slightly modified to reverse the order it searches for spotify
+# as it sometimes for whatever reason seems to create another playback
+# when casting to a device
+# then the last playback is desired and not the first
 import subprocess
 import os
 import sys
-x=0
-y=0
 env = os.environ
 env['LANG'] = 'en_US'
 app = '"Spotify"'
 pactl = subprocess.check_output(['pactl', 'list', 'sink-inputs'], env=env).decode().strip().split()
+x=len(pactl) #original: x=0
+y=0
 if app in pactl:
-    for e in pactl:
-        x += 1
+    for e in reversed(pactl): #original: for e in pactl:
+        x -= 1 #original: x += 1
         if e == app:
             break
     for i in pactl[0 : x -1 ]:
