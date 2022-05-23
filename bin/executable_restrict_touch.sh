b24775c718a5fe59303e6cc561b0bb54aba33643
Jorik Cronenberg <jcronenberg@suse.de>
2022-05-23 14:02:03 +0200

    Add restrict_touch bash script

---
+++ bin/executable_restrict_touch.sh
@@ -0,0 +1,15 @@
+if [ "$1" = "" ]; then 
+    exit 1
+fi
+
+ids=$(xinput --list | awk -v search="$1" \
+    '$0 ~ search {match($0, /id=[0-9]+/);\
+                  if (RSTART) \
+                    print substr($0, RSTART+3, RLENGTH-3)\
+                 }'\
+     )
+
+for i in $ids
+do
+    xinput map-to-output $i HDMI-0
+done
