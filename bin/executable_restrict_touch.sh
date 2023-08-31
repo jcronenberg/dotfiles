7a80ab675310f8ec2a69d99337a0df823a78f0e6
Jorik Cronenberg <jcronenberg@suse.de>
2023-08-31 23:10:28 +0200

    Fix restrict_touch script

--- bin/executable_restrict_touch.sh
+++ bin/executable_restrict_touch.sh
@@ -2,7 +2,7 @@ if [ "$1" = "" ]; then
     exit 1
 fi
 
-ids=$(xinput --list | awk -v search="$1" \
+ids=$(xinput --list | grep pointer | awk -v search="$1" \
     '$0 ~ search {match($0, /id=[0-9]+/);\
                   if (RSTART) \
                     print substr($0, RSTART+3, RLENGTH-3)\
